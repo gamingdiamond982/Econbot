58f3822049feed825f1decae01166bbfbfdceb5d
Dodowarrior44 <45238920+Dodowarrior44@users.noreply.github.com>
2019-12-01 17:03:54 -0500

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,1 +1,8 @@
+# We need a token, and account, but mobil can add that - since he's ES
 
+import discord
+import logging
+import os
+import random
+import csv
+# from keep_alive import keepAlive - Need to set up keep_alive and uptimerobot.com
