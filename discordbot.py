7f086c9e23bc31155cbd65e65d7e095e75439a5d
Dodowarrior44 <45238920+Dodowarrior44@users.noreply.github.com>
2019-12-16 10:07:21 -0500

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,36 +5,32 @@ import logging
 
 # from keep_alive import keepAlive - Need to set up keep_alive and uptimerobot.com
 
-TOKEN = os.environ.get("TOKEN") # Gets token
+TOKEN = os.environ.get("TOKEN") # Gets token - we don't have the token yet
 logging.basicConfig(level=logging.INFO)
 client = discord.Client()
 
 # Declaring Variables
 
-prefix = "e;" # The prefix
+prefix = "ee" # The prefix
 
-server = "simdem" # Just a placeholder, IDK simdem's server name id thing
+server = "simdem" # Just a placeholder, IDK simdem's server id
 
 # The Progam
 
 @client.event
 async def on_message(message): # When a message on a channel the bot can see is put out
-  if message.content == prefix + "hello": # Just some test stuff
-    await message.channel.send("Hello " + message.author.mention)
   if message.content == prefix + "balance":
     try:
       print(str(get_account_balance(message.author.id, server)))
     except:
       await message.channel.send("An error has occured. Are you sure you have an account made?")
       
-  # The help command
-    
-  if message.content == prefix + "help":
-        embed = discord.Embed(title="help", description="The help page - you're here now!", color=message.author.color)
-      
+if message.content == prefix + "help": # The help command
+  embed = discord.Embed(title="help", description="The help page - you're here now!", color=message.author.color) # Sets up the embed
+  embed.add_field(name="Prefix", value="Prefix all statements with " + prefix + '"', inline=False) # Adds lines to the embed
 
 @client.event
-async def on_ready():
+async def on_ready(): # Lets the console owner see it's booted up
   print('Logged in as')
   print(client.user.name)
   print(client.user.id)
@@ -44,6 +40,6 @@ keepAlive() # Keeps repl from shutting down
 
 while True:
   try:
-    client.run(TOKEN, bot=True)
+    client.run(TOKEN, bot=True) # Runs the bot
   except:
     pass
